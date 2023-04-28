9b102e8d83d23a5dfa41fa13a8a9ee2e82616feb
Prestin Liu <liuprestin@gmail.com>
2023-04-27 22:39:35 -0700

    added findKeyByValue.js and some tests

--- letterPositions.js
+++ letterPositions.js
@@ -16,6 +16,9 @@ function letterPositions(sentence){
         let temp_arr = [x];
         console.log(collectedLetters[x])
         //collectedLetters[x] = collectedLetters[x].push(x) || temp_arr; //increment by one or if it doesn't exist yet
+
+        // could instead use .indexOF() until it returns a -1 ; replace each instance of a letter with a 1 at that index since we have it 
+        // continue through the loop 
     }
 
 
