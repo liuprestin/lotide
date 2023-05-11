285e8a96fbfdf4cf83e8a4f2d373398d733a63f3
Prestin Liu <liuprestin@gmail.com>
2023-05-10 21:05:58 -0700

    ES6 syntax for index.js

--- index.js
+++ index.js
@@ -5,10 +5,22 @@ const middle = require('./middle');
 const flatten = require('./flatten');
 const joinList = require('./joinList');
 
+/*
+// kept this for Node 12 compatibility
 module.exports = {
   head:   head,
   tail:   tail,
   middle: middle,
   flatten: flatten,
   joinList: joinList
+};
+*/
+
+//ES6 syntax
+module.exports = {
+    head,
+    tail,
+    middle,
+    flatten,
+    joinList
 };
