b4aaf6c85627e415b5c1df47e770d4d64ed27595
Prestin Liu <liuprestin@gmail.com>
2023-05-10 20:32:50 -0700

    starting refactor for tail and its tests

---
+++ test/tailTest.js
@@ -0,0 +1,14 @@
+const assert = require("chai").assert;
+const tail = require("../head");
+
+describe("#tail", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(tail([1, 2, 3]), 3);
+  });
+});
+
+//test each value
+/*
+assertEqual(tail([1,2,3,4])[0], [2,3,4][0]);
+assertEqual(tail(["Hello", "World"])[0], "World");
+*/
