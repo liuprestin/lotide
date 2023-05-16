a0233b39b50acd3c5a51b8dc3cbebf77416461c0
Prestin Liu <liuprestin@gmail.com>
2023-05-16 15:46:45 -0700

    cleaning up comments in eqArrays.js

--- eqArrays.js
+++ eqArrays.js
@@ -1,12 +1,10 @@
-//Console Assertion function that checks Arrays
-// We also validate each value 
+// Console Assertion function that checks each 
+// value of two arrays to see if they are equal
 // returns true or false
 function eqArrays(actual, expected){
     if (actual.length !== expected.length) {
         return false;
     }
-
-    //here we assume each array is same length
     for (let i = 0; i < actual.length; i++){
         if( actual[i] !== expected[i]) {
             return false;
