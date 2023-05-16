250b7811a6c89da956e256d6444566c6057421ef
Prestin Liu <liuprestin@gmail.com>
2023-05-16 15:53:40 -0700

    cleaning up comments

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,21 +1,11 @@
-
-// ES6 way of importing 
-//import {assertArraysEqual} from "./assertArraysEqual.js"
-//import { eqArrays } from "./eqArrays.js"
-
-/*
-console.log(middle([1])); //should return []
-console.log(middle([1,2,3,4])); //should print a message
-*/
-
-const assert = require('chai').assert;
-const middle   = require('../middle');
+const assert = require("chai").assert;
+const middle = require("../middle");
 
 describe("#middle", () => {
   it("returns empty array for [1]", () => {
     assert.strictEqual(middle([1]), []);
   });
   it("returns [2,3] as even amount of element", () => {
-    assert.strictEqual(middle([1,2,3,4]), [2,3]);
+    assert.strictEqual(middle([1, 2, 3, 4]), [2, 3]);
   });
 });
