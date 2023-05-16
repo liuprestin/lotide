d3bb76fb0d883570eb6d3aa0417b322b42f4018c
Prestin Liu <liuprestin@gmail.com>
2023-05-16 15:50:38 -0700

    cleaned up comments

--- test/headTest.js
+++ test/headTest.js
@@ -5,6 +5,7 @@ describe("#head", () => {
   it("returns 1 for [1, 2, 3]", () => {
     assert.strictEqual(head([1, 2, 3]), 1);
   });
+  
 });
 
 //OLD test - note: bad practice to leave commented out code like this...
