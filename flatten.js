aab10e86dab1f833929f31d8b662cbd024ef9ef6
Prestin Liu <liuprestin@gmail.com>
2023-05-16 16:04:12 -0700

    cleaning up comments in flatten.js

--- flatten.js
+++ flatten.js
@@ -1,15 +1,9 @@
 // Take in an array and remove
 // embedded arrays into one array
-
 function flatten(arr) {
   let rebuild = [];
 
   for (let ele of arr) {
-    //hmm how does the spread function work?
-    // check type
-    // spread if its an array
-    // Push()
-    
     if (ele instanceof Array) {
       rebuild.push(...flatten(ele));
     } else {
