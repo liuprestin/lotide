1b08a6377452da6416b53a0bbd9a2e36d5db24e4
Prestin Liu <liuprestin@gmail.com>
2023-05-16 16:04:27 -0700

    cleaning up comments in head.js

--- head.js
+++ head.js
@@ -1,11 +1,5 @@
 //similar to: https://lodash.com/docs/4.17.15#head
 
-// ES6 async way: 
-//import {assertEqual} from "./assertEqual.js";
-
-// Classic way: note not needed if we have Mocha/chai
-// const assertEqual = require('./assertEqual');
-
 // Given an array return a copy of the 1st element 
 // it must be a copy of the value and not the original 
 // (ie. does not mutate)
@@ -13,7 +7,5 @@ function head(arr){
     let value = arr.slice(0, -(arr.length - 1)).pop();
     return value;
 }
-
-
 
 module.exports = head;
